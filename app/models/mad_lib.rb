4e54228dac039ddbf3861a36d530bd860618c49f
Gabriel Zurita <gabezurita@gmail.com>
2018-08-19 16:54:24 -0400

    add methods to pass 3rd test's simple madlib scenario's text, though failing with more complicated scenario

--- app/models/mad_lib.rb
+++ app/models/mad_lib.rb
@@ -1,6 +1,8 @@
 require 'byebug'
 
 class MadLib < ApplicationRecord
+  attr_reader :formatted_fields
+
   # this finds the parts of speech (fields) within the given text from the MadLib model
   def field_reg_ex
     /\{([^}]+)\}/
@@ -14,8 +16,14 @@ class MadLib < ApplicationRecord
     find_fields.each_with_object(Hash.new(0)) { |field, count| count[field] += 1 }
   end
 
+  def format_fields
+    @formatted_fields = []
+    count_fields.each {|field, count| @formatted_fields << "#{field.first.capitalize} (#{count}):"}
+  end
+
   def has_field?(field)
     byebug
-    field
+    format_fields
+    @formatted_fields.include? field
   end
 end
