efc1d9d24fc5458fcb688fa9d58f61907b32eb9d
Gabriel Zurita <gabezurita@gmail.com>
2018-08-19 16:47:35 -0400

    add methods to find fields in text and count how often they occur

--- app/models/mad_lib.rb
+++ app/models/mad_lib.rb
@@ -1,8 +1,21 @@
 require 'byebug'
 
 class MadLib < ApplicationRecord
+  # this finds the parts of speech (fields) within the given text from the MadLib model
+  def field_reg_ex
+    /\{([^}]+)\}/
+  end
+
+  def find_fields
+    text.scan(field_reg_ex)
+  end
+
+  def count_fields
+    find_fields.each_with_object(Hash.new(0)) { |field, count| count[field] += 1 }
+  end
 
   def has_field?(field)
     byebug
+    field
   end
 end
